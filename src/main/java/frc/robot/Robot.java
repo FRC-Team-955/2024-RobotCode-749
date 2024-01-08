b86ad396d05abc04cc40ade54b5f96cfded03e6c
WackyHamster <155921433+WackyHamster@users.noreply.github.com>
2024-01-07 16:14:53 -0800

    Launcher subsystem
    Contains launch and intake function.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,11 +3,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.IOConstants;
+import frc.robot.subsystems.Launcher;
 
 public class Robot {
     private final CommandXboxController controller = new CommandXboxController(IOConstants.controllerPort);
 
     /* Subsystems */
+    private final Launcher launcher = new Launcher();
 
     public Robot() {
         setDefaultCommands();
@@ -18,6 +20,8 @@ public class Robot {
     }
 
     private void configureBindings() {
+        controller.x().onTrue(launcher.startLaunchingCommand().withTimeout(5).andThen(launcher.stopCommand()));
+        controller.b().onTrue(launcher.startIntakeCommand().withTimeout(5).andThen(launcher.stopCommand()));
     }
 
     public Command getAutonomousCommand() {
