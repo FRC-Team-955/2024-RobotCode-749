1088e8a4efd5b7ebc34dc737a604c4a874f3453a
FarisAri <cv.raiderbot@gmail.com>
2024-01-07 16:00:45 -0800

    Drivebase is the new default command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,11 +3,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.IOConstants;
+import frc.robot.subsystems.Drivebase;
 
 public class Robot {
     private final CommandXboxController controller = new CommandXboxController(IOConstants.controllerPort);
 
     /* Subsystems */
+    private final Drivebase drivebase = new Drivebase();
+    
 
     public Robot() {
         setDefaultCommands();
@@ -15,6 +18,7 @@ public class Robot {
     }
 
     private void setDefaultCommands() {
+        drivebase.setDefaultCommand(drivebase.run(() -> drivebase.arcadeDrive(controller.getLeftY(), controller.getRightX())));
     }
 
     private void configureBindings() {
