fd80ba29636455d9915c682dfc42280459028aa4
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2024-01-10 18:52:24 -0800

    Format

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -1,17 +1,16 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.DrivebaseConstants;
 
 public class Drivebase extends SubsystemBase {
-    private CANSparkMax leftMotorLeader = new CANSparkMax(DrivebaseConstants.leftMotorLeader, MotorType.kBrushed);
-    private CANSparkMax leftMotorFollower = new CANSparkMax(DrivebaseConstants.leftMotorFollower, MotorType.kBrushed);
-    private CANSparkMax rightMotorLeader = new CANSparkMax(DrivebaseConstants.rightMotorLeader, MotorType.kBrushed);
-    private CANSparkMax rightMotorFollower = new CANSparkMax(DrivebaseConstants.rightMotorFollower, MotorType.kBrushed);
+    private final CANSparkMax leftMotorLeader = new CANSparkMax(DrivebaseConstants.leftMotorLeader, MotorType.kBrushed);
+    private final CANSparkMax leftMotorFollower = new CANSparkMax(DrivebaseConstants.leftMotorFollower, MotorType.kBrushed);
+    private final CANSparkMax rightMotorLeader = new CANSparkMax(DrivebaseConstants.rightMotorLeader, MotorType.kBrushed);
+    private final CANSparkMax rightMotorFollower = new CANSparkMax(DrivebaseConstants.rightMotorFollower, MotorType.kBrushed);
 
     public Drivebase() {
         leftMotorFollower.follow(leftMotorLeader);
@@ -20,7 +19,7 @@ public class Drivebase extends SubsystemBase {
 
     private final DifferentialDrive drive = new DifferentialDrive(leftMotorLeader, rightMotorLeader);
 
-    public void arcadeDrive(double speed, double rotation){
+    public void arcadeDrive(double speed, double rotation) {
         drive.arcadeDrive(rotation, speed);
     }
 }
