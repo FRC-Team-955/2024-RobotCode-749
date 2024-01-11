89e9856b2570ed34838e4c33eb2780024d089b0b
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2024-01-10 19:42:03 -0800

    Improved launch and intake buttons to allow toggling them (#5)

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -10,24 +10,32 @@ public class Launcher extends SubsystemBase {
     private final CANSparkMax feederMotor = new CANSparkMax(LauncherConstants.feederId, MotorType.kBrushed);
     private final CANSparkMax bottomMotor = new CANSparkMax(LauncherConstants.launcherId, MotorType.kBrushed);
 
-    public Command startLaunchingCommand() {
-        return this.runOnce(() -> {
-            feederMotor.set(LauncherConstants.launchingSpeed);
-            bottomMotor.set(LauncherConstants.launchingSpeed);
-        });
+    private void startLaunching() {
+        feederMotor.set(LauncherConstants.launchingSpeed);
+        bottomMotor.set(LauncherConstants.launchingSpeed);
     }
 
-    public Command startIntakeCommand() {
-        return this.runOnce(() -> {
-            feederMotor.set(LauncherConstants.feederIntakeSpeed);
-            bottomMotor.set(LauncherConstants.launcherIntakeSpeed);
-        });
+    private void startIntake() {
+        feederMotor.set(LauncherConstants.feederIntakeSpeed);
+        bottomMotor.set(LauncherConstants.launcherIntakeSpeed);
     }
 
-    public Command stopCommand() {
-        return this.runOnce(() -> {
-            feederMotor.stopMotor();
-            bottomMotor.stopMotor();
-        });
+    private void stopMotors() {
+        feederMotor.stopMotor();
+        bottomMotor.stopMotor();
+    }
+
+    public Command launchCommand() {
+        return this.startEnd(
+                this::startLaunching,
+                this::stopMotors
+        );
+    }
+
+    public Command intakeCommand() {
+        return this.startEnd(
+                this::startIntake,
+                this::stopMotors
+        );
     }
 }
