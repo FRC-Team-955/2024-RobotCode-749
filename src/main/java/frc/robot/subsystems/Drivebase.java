5f2190358af798591084430e18c2f2358cee363c
harrison <harrisonsterlingscott@gmail.com>
2024-01-10 18:34:05 -0800

    Removed motor controller group
    Replaced then with motors following other motors

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -4,20 +4,21 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.DrivebaseConstants;
 
 public class Drivebase extends SubsystemBase {
-    private CANSparkMax leftMotor1 = new CANSparkMax(DrivebaseConstants.leftMotorId1, MotorType.kBrushed);
-    private CANSparkMax leftMotor2 = new CANSparkMax(DrivebaseConstants.leftMotorId2, MotorType.kBrushed);
-    private CANSparkMax rightMotor1 = new CANSparkMax(DrivebaseConstants.rightMotorId1, MotorType.kBrushed);
-    private CANSparkMax rightMotor2 = new CANSparkMax(DrivebaseConstants.rightMotorId2, MotorType.kBrushed);
+    private CANSparkMax leftMotorLeader = new CANSparkMax(DrivebaseConstants.leftMotorLeader, MotorType.kBrushed);
+    private CANSparkMax leftMotorFollower = new CANSparkMax(DrivebaseConstants.leftMotorFollower, MotorType.kBrushed);
+    private CANSparkMax rightMotorLeader = new CANSparkMax(DrivebaseConstants.rightMotorLeader, MotorType.kBrushed);
+    private CANSparkMax rightMotorFollower = new CANSparkMax(DrivebaseConstants.rightMotorFollower, MotorType.kBrushed);
 
-    private MotorControllerGroup leftMotors = new MotorControllerGroup(leftMotor1, leftMotor2);
-    private MotorControllerGroup rightMotors = new MotorControllerGroup(rightMotor1, rightMotor2);
+    public Drivebase() {
+        leftMotorFollower.follow(leftMotorLeader);
+        rightMotorFollower.follow(rightMotorLeader);
+    }
 
-    private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
+    private final DifferentialDrive drive = new DifferentialDrive(leftMotorLeader, rightMotorLeader);
 
     public void arcadeDrive(double speed, double rotation){
         drive.arcadeDrive(rotation, speed);
