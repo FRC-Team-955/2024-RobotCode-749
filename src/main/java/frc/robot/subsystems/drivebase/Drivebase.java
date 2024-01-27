2c018593c801cb785d7d43a946d68e8ffa135d94
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2024-01-26 20:18:23 -0800

    Remove redundant arcadeDrive in swerve mode

--- src/main/java/frc/robot/subsystems/drivebase/Drivebase.java
+++ src/main/java/frc/robot/subsystems/drivebase/Drivebase.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -101,8 +100,6 @@ public class Drivebase extends SubsystemBase {
 
             if (Math.abs(x) > DrivebaseConstants.swerveModeDeadzone || Math.abs(y) > DrivebaseConstants.swerveModeDeadzone) {
                 swerveModeSetpoint = Math.toDegrees(Math.atan2(-x, y));
-            } else {
-                arcadeDrive(controller.getLeftY(), 0);
             }
 
             swerveModePID.setSetpoint(swerveModeSetpoint);
