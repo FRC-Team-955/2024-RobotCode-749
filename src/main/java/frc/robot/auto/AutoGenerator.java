2da0c0469f0855c1227ff0a8131f4b4a56567556
NoobKilla5412 <brandoncarlrobertson@gmail.com>
2024-02-15 19:22:23 -0800

    Clean up auto

--- src/main/java/frc/robot/auto/AutoGenerator.java
+++ src/main/java/frc/robot/auto/AutoGenerator.java
@@ -37,69 +37,51 @@ public class AutoGenerator {
         return startingPoints;
     });
 
-    private static final SendableChooser<Note> notes = Util.make(() -> {
-        var notes = new SendableChooser<Note>();
-        notes.setDefaultOption("Wing Top", Note.WingTopNote);
-        notes.addOption("Wing Middle", Note.WingMiddleNote);
-        notes.addOption("Wing Bottom", Note.WingBottomNote);
-        notes.addOption("Midfield Top", Note.MidfieldTopNote);
-        notes.addOption("Midfield Upper Middle", Note.MidfieldUpperMiddleNote);
-        notes.addOption("Midfield Middle", Note.MidfieldMiddleNote);
-        notes.addOption("Midfield Lower Middle", Note.MidfieldLowerMiddleNote);
-        notes.addOption("Midfield Bottom", Note.MidfieldBottomNote);
-        return notes;
-    });
+    // private static final SendableChooser<Note> notes = Util.make(() -> {
+    //     var notes = new SendableChooser<Note>();
+    //     notes.setDefaultOption("Wing Top", Note.WingTopNote);
+    //     notes.addOption("Wing Middle", Note.WingMiddleNote);
+    //     notes.addOption("Wing Bottom", Note.WingBottomNote);
+    //     notes.addOption("Midfield Top", Note.MidfieldTopNote);
+    //     notes.addOption("Midfield Upper Middle", Note.MidfieldUpperMiddleNote);
+    //     notes.addOption("Midfield Middle", Note.MidfieldMiddleNote);
+    //     notes.addOption("Midfield Lower Middle", Note.MidfieldLowerMiddleNote);
+    //     notes.addOption("Midfield Bottom", Note.MidfieldBottomNote);
+    //     return notes;
+    // });
 
-    private static Command followPath(String pathName) {
-        return AutoBuilder.followPath(PathPlannerPath.fromPathFile(pathName));
-    }
+    // private static Command followPath(String pathName) {
+    //     return AutoBuilder.followPath(PathPlannerPath.fromPathFile(pathName));
+    // }
 
     public static Command generateAuto(Drivebase drivebase){
         var startingPoint = startingPoints.getSelected();
-        // var note = AutoGenerator.notes.getSelected();
         var commands = new SequentialCommandGroup();
 
+        PathPlannerPath path = null;
         switch (startingPoint) {
             case Top -> {
-                var path = PathPlannerPath.fromPathFile("Starting top to subwoofer");
-                commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
-                commands.addCommands(AutoBuilder.followPath(path));
+                path = PathPlannerPath.fromPathFile("Starting top to subwoofer");
             }
             case Middle -> {
-                var path = PathPlannerPath.fromPathFile("Starting middle to subwoofer");
-                commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
-                commands.addCommands(AutoBuilder.followPath(path));
+                path = PathPlannerPath.fromPathFile("Starting middle to subwoofer");
             }
             case Bottom -> {
-                var path = PathPlannerPath.fromPathFile("Starting bottom to subwoofer");
-                commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
-                commands.addCommands(AutoBuilder.followPath(path));
+                path = PathPlannerPath.fromPathFile("Starting bottom to subwoofer");
             }
         }
+        commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
+        commands.addCommands(AutoBuilder.followPath(path));
 
         if (messUpMidfieldTopNote.getBoolean(true)) {
-            var path = PathPlannerPath.fromPathFile("Mess with Midfield(1) note");
-            commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
-            commands.addCommands(AutoBuilder.followPath(path));
+            path = PathPlannerPath.fromPathFile("Mess with Midfield(1) note");
         } else {
-            var path = PathPlannerPath.fromPathFile("Go near midfield (1)");
-            commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
-            commands.addCommands(AutoBuilder.followPath(path));
+            path = PathPlannerPath.fromPathFile("Go near midfield (1)");
         }
+        commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
+        commands.addCommands(AutoBuilder.followPath(path));
 
         Rotation2d rotation = Rotation2d.fromDegrees(-90);
-
-        // switch (startingPoint) {
-        //     case Top -> rotation = new Rotation2d(-90);
-        //     case Middle -> rotation = new Rotation2d(-90);
-        //     case Bottom -> rotation = new Rotation2d(90);
-        // }
-
-        // commands.addCommands(drivebase.pathfindCommand(Util.flipIfNeeded(messUpMidfieldTopNote.getBoolean(true)         ? new Pose2d(7.80, 7.40, rotation) : new Pose2d(7.30, 7.40, rotation)),
-        //                      drivebase.pathfindCommand(Util.flipIfNeeded(messUpMidfieldUpperMiddleNote.getBoolean(true) ? new Pose2d(7.80, 5.75, rotation) : new Pose2d(7.30, 5.75, rotation)),
-        //                      drivebase.pathfindCommand(Util.flipIfNeeded(messUpMidfieldMiddleNote.getBoolean(true)      ? new Pose2d(7.80, 4.10, rotation) : new Pose2d(7.30, 4.10, rotation)),
-        //                      drivebase.pathfindCommand(Util.flipIfNeeded(messUpMidfieldLowerMiddleNote.getBoolean(true) ? new Pose2d(7.80, 2.50, rotation) : new Pose2d(7.30, 2.50, rotation)),
-        //                      drivebase.pathfindCommand(Util.flipIfNeeded(messUpMidfieldBottomNote.getBoolean(true)      ? new Pose2d(7.80, 0.80, rotation) : new Pose2d(7.30, 0.80, rotation))))))));
 
         List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
             Util.flipIfNeededNow(messUpMidfieldTopNote.getBoolean(true)          ? new Pose2d(7.70, 7.40, rotation) : new Pose2d(7.30, 7.40, rotation)),
@@ -109,66 +91,14 @@ public class AutoGenerator {
             Util.flipIfNeededNow(messUpMidfieldBottomNote.getBoolean(true)       ? new Pose2d(7.70, 0.80, rotation) : new Pose2d(7.30, 0.80, rotation))
         );
 
-        PathPlannerPath path = new PathPlannerPath(
+        path = new PathPlannerPath(
             bezierPoints,
             new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI),
             new GoalEndState(0.0, Rotation2d.fromDegrees(90))
         );
 
         path.preventFlipping = true;
-        // commands.addCommands(drivebase.setPoseCommand(GeometryUtil.flipFieldPose(path.getStartingDifferentialPose())));
         commands.addCommands(AutoBuilder.followPath(path));
-
-        //     switch (note) {
-            //         case WingTopNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to top note"),
-            //                 followPath("Top note to subwoofer")
-            //                 );
-            //         }
-            //         case WingMiddleNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to middle note"),
-            //                 followPath("Middle note to subwoofer")
-            //             );
-            //         }
-            //         case WingBottomNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to bottom note"),
-            //                 followPath("Bottom note to subwoofer")
-            //             );
-            //         }
-            //         case MidfieldTopNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to midfield(1) note"),
-            //                 followPath("Midfield(1) to subwoofer")
-            //             );
-            //         }
-            //         case MidfieldUpperMiddleNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to midfield(2) note"),
-            //                 followPath("Midfield(2) to subwoofer")
-            //             );
-            //         }
-            //         case MidfieldMiddleNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to midfield(3) note"),
-            //                 followPath("Midfield(3) to subwoofer")
-            //             );
-            //         }
-            //         case MidfieldLowerMiddleNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to midfield(4) note"),
-            //                 followPath("Midfield(4) to subwoofer")
-            //             );
-            //         }
-            //         case MidfieldBottomNote -> {
-            //             commands.addCommands(
-            //                 followPath("Subwoofer to midfield(5) note"),
-            //                 followPath("Midfield(5) to subwoofer")
-            //             );
-            //         }
-        //     }
 
         return commands;
     }
@@ -176,6 +106,6 @@ public class AutoGenerator {
     public static void initializeShuffleboard() {
         tab.add("Starting Point", startingPoints);
 
-        tab.add("Notes", notes);
+        // tab.add("Notes", notes);
     }
 }
