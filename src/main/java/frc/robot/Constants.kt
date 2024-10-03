0aa06610ac4a62c064404f91d20c97e6cbec6331
amsam0 <44983869+amsam0@users.noreply.github.com>
2024-10-03 16:43:21 -0700

    Fix speeds

--- src/main/java/frc/robot/Constants.kt
+++ src/main/java/frc/robot/Constants.kt
@@ -96,9 +96,9 @@ object Constants {
         const val pivotD = 0.0
         val pivotFFg = ifSimElse(0.01, 0.35)
 
-        const val intakeSpeed = 0.75
-        const val handoffSpeed = -0.75
-        const val handoffTimeout = 0.3
+        const val intakeSpeed = 1.0
+        const val handoffSpeed = -0.5
+        const val handoffTimeout = 0.2
         const val ejectSpeed = -1.0
         const val ejectTimeout = 0.5
         const val ejectIntakeTimeout = 0.15
